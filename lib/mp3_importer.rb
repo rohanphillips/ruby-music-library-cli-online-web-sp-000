54c10518e93895e3b6bbdb3bd947143ccde02e15
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-12 16:52:26 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -35,7 +35,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-
+    response = ""
     while response != "exit"
       response = gets.strip
       binding.pry
