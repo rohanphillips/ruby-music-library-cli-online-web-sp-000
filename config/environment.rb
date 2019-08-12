ed7dcd7a98c1b4da9d3fa6b92de1dd38ccebfcf0
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-12 15:08:36 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,17 @@ Bundler.require
 
 module Concerns
   module Findable
+    module ClassMethods
+      def find_by_name(name)
+        all.detect{|a| a.name == name}
+        #returns nil if not found
+      end
+    end
+    module InstanceMethods
+      def song_exists?(collection, song)
+        collection.detect{|n| n == song}
+      end
+    end
   end
 end
 
