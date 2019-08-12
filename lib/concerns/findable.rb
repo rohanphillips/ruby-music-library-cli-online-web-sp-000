4940b690a4730c32a908414b0e87f8cd677a73ec
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-12 15:01:42 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,17 +1,1 @@
-module Concerns
-  module Findable
-      module ClassMethods
-        def find_by_name(name)
-          all.detect{|a| a.name == name}
-          #returns nil if not found
-        end
 
-
-      end
-      module InstanceMethods
-        def song_exists?(collection, song)
-          collection.detect{|n| n == song}
-        end
-      end
-  end
-end
