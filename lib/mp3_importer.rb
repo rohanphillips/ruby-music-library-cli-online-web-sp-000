c1f5db136ec169d01c41de17f0350c8b5af1c134
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-12 17:34:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -83,7 +83,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song = gets.strip
-    binding.pry
+    #binding.pry
     puts "Playing #{list_songs[song.to_i]}"
   end
 end
