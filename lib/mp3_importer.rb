4c03061dd9266812b6ec54068092d9a413325e2d
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-12 18:06:18 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -85,9 +85,9 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    songnumber = gets.strip
-    if songnumber.to_i < list_all_songs.size
-      song = list_all_songs[songnumber.to_i - 1]
+    songnumber = gets.strip.to_i
+    if songnumber < list_all_songs.size
+      song = list_all_songs[songnumber - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
