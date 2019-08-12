f10df44858efb3fb82d24699fef6c30f4b9b6321
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-12 13:00:02 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
+
+class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@artists = []
+
+  def initialize
+    self.class.all << self
+    @songs = []
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+end
